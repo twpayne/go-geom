745caf0b2989dc131b44a9f066a593f9585bcd51
Tom Payne <twpayne@gmail.com>
2014-03-29 21:02:03 +0100

    Add WKB encoding

---
+++ geom/encoding/wkb/wkb.go
@@ -0,0 +1,366 @@
+package wkb
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+
+	"github.com/twpayne/gogeom2/geom"
+)
+
+const (
+	wkbXDR = 0
+	wkbNDR = 1
+)
+
+type ByteOrder binary.ByteOrder
+
+var (
+	XDR = ByteOrder(binary.BigEndian)
+	NDR = ByteOrder(binary.LittleEndian)
+)
+
+type ErrUnknownByteOrder byte
+
+func (e ErrUnknownByteOrder) Error() string {
+	return fmt.Sprintf("wkb: unknown byte order: %b", byte(e))
+}
+
+type Type uint32
+
+type ErrUnknownType Type
+
+func (e ErrUnknownType) Error() string {
+	return fmt.Sprintf("wkb: unknown type: %u", uint(e))
+}
+
+type ErrUnsupportedType Type
+
+func (e ErrUnsupportedType) Error() string {
+	return fmt.Sprintf("wkb: unsupported type: %u", uint(e))
+}
+
+const (
+	wkbPoint              = 1
+	wkbLineString         = 2
+	wkbPolygon            = 3
+	wkbMultiPoint         = 4
+	wkbMultiLineString    = 5
+	wkbMultiPolygon       = 6
+	wkbGeometryCollection = 7
+	wkbPolyhedralSurface  = 15
+	wkbTIN                = 16
+	wkbTriangle           = 17
+)
+
+const (
+	wkbXY   = 0
+	wkbXYZ  = 1000
+	wkbXYM  = 2000
+	wkbXYZM = 3000
+)
+
+const (
+	Point              = Type(wkbPoint + wkbXY)
+	LineString         = Type(wkbLineString + wkbXY)
+	Polygon            = Type(wkbPolygon + wkbXY)
+	MultiPoint         = Type(wkbMultiPoint + wkbXY)
+	MultiLineString    = Type(wkbMultiLineString + wkbXY)
+	MultiPolygon       = Type(wkbMultiPolygon + wkbXY)
+	GeometryCollection = Type(wkbGeometryCollection + wkbXY)
+	PolyhedralSurface  = Type(wkbPolyhedralSurface + wkbXY)
+	TIN                = Type(wkbTIN + wkbXY)
+	Triangle           = Type(wkbTriangle + wkbXY)
+
+	PointZ              = Type(wkbPoint + wkbXYZ)
+	LineStringZ         = Type(wkbLineString + wkbXYZ)
+	PolygonZ            = Type(wkbPolygon + wkbXYZ)
+	MultiPointZ         = Type(wkbMultiPoint + wkbXYZ)
+	MultiLineStringZ    = Type(wkbMultiLineString + wkbXYZ)
+	MultiPolygonZ       = Type(wkbMultiPolygon + wkbXYZ)
+	GeometryCollectionZ = Type(wkbGeometryCollection + wkbXYZ)
+	PolyhedralSurfaceZ  = Type(wkbPolyhedralSurface + wkbXYZ)
+	TINZ                = Type(wkbTIN + wkbXYZ)
+	TriangleZ           = Type(wkbTriangle + wkbXYZ)
+
+	PointM              = Type(wkbPoint + wkbXYM)
+	LineStringM         = Type(wkbLineString + wkbXYM)
+	PolygonM            = Type(wkbPolygon + wkbXYM)
+	MultiPointM         = Type(wkbMultiPoint + wkbXYM)
+	MultiLineStringM    = Type(wkbMultiLineString + wkbXYM)
+	MultiPolygonM       = Type(wkbMultiPolygon + wkbXYM)
+	GeometryCollectionM = Type(wkbGeometryCollection + wkbXYM)
+	PolyhedralSurfaceM  = Type(wkbPolyhedralSurface + wkbXYM)
+	TINM                = Type(wkbTIN + wkbXYM)
+	TriangleM           = Type(wkbTriangle + wkbXYM)
+
+	PointZM              = Type(wkbPoint + wkbXYZM)
+	LineStringZM         = Type(wkbLineString + wkbXYZM)
+	PolygonZM            = Type(wkbPolygon + wkbXYZM)
+	MultiPointZM         = Type(wkbMultiPoint + wkbXYZM)
+	MultiLineStringZM    = Type(wkbMultiLineString + wkbXYZM)
+	MultiPolygonZM       = Type(wkbMultiPolygon + wkbXYZM)
+	GeometryCollectionZM = Type(wkbGeometryCollection + wkbXYZM)
+	PolyhedralSurfaceZM  = Type(wkbPolyhedralSurface + wkbXYZM)
+	TINZM                = Type(wkbTIN + wkbXYZM)
+	TriangleZM           = Type(wkbTriangle + wkbXYZM)
+)
+
+func (t Type) layout() (geom.Layout, error) {
+	switch t {
+	case Point, LineString, Polygon, MultiPoint, MultiLineString, MultiPolygon, GeometryCollection, PolyhedralSurface, TIN, Triangle:
+		return geom.XY, nil
+	case PointZ, LineStringZ, PolygonZ, MultiPointZ, MultiLineStringZ, MultiPolygonZ, GeometryCollectionZ, PolyhedralSurfaceZ, TINZ, TriangleZ:
+		return geom.XYZ, nil
+	case PointM, LineStringM, PolygonM, MultiPointM, MultiLineStringM, MultiPolygonM, GeometryCollectionM, PolyhedralSurfaceM, TINM, TriangleM:
+		return geom.XYM, nil
+	case PointZM, LineStringZM, PolygonZM, MultiPointZM, MultiLineStringZM, MultiPolygonZM, GeometryCollectionZM, PolyhedralSurfaceZM, TINZM, TriangleZM:
+		return geom.XYZM, nil
+	default:
+		return geom.Layout(0), ErrUnknownType(t)
+	}
+}
+
+func readFlatCoords0(r io.Reader, byteOrder ByteOrder, stride int) ([]float64, error) {
+	coord := make([]float64, stride)
+	if err := binary.Read(r, byteOrder, &coord); err != nil {
+		return nil, err
+	}
+	return coord, nil
+}
+
+func readFlatCoords1(r io.Reader, byteOrder ByteOrder, stride int) ([]float64, error) {
+	var n uint32
+	if err := binary.Read(r, byteOrder, &n); err != nil {
+		return nil, err
+	}
+	flatCoords := make([]float64, int(n)*stride)
+	if err := binary.Read(r, byteOrder, &flatCoords); err != nil {
+		return nil, err
+	}
+	return flatCoords, nil
+}
+
+func readFlatCoords2(r io.Reader, byteOrder ByteOrder, stride int) ([]float64, []int, error) {
+	var n uint32
+	if err := binary.Read(r, byteOrder, &n); err != nil {
+		return nil, nil, err
+	}
+	var flatCoordss []float64
+	var ends []int
+	for i := 0; i < int(n); i++ {
+		flatCoords, err := readFlatCoords1(r, byteOrder, stride)
+		if err != nil {
+			return nil, nil, err
+		}
+		flatCoordss = append(flatCoordss, flatCoords...)
+		ends = append(ends, len(flatCoords))
+	}
+	return flatCoordss, ends, nil
+}
+
+func read(r io.Reader) (geom.T, error) {
+
+	var wkbByteOrder byte
+	if err := binary.Read(r, binary.LittleEndian, &wkbByteOrder); err != nil {
+		return nil, err
+	}
+	var byteOrder ByteOrder
+	switch wkbByteOrder {
+	case wkbXDR:
+		byteOrder = XDR
+	case wkbNDR:
+		byteOrder = NDR
+	default:
+		return nil, ErrUnknownByteOrder(wkbByteOrder)
+	}
+
+	var wkbGeometryType uint32
+	if err := binary.Read(r, byteOrder, &wkbGeometryType); err != nil {
+		return nil, err
+	}
+
+	t := Type(wkbGeometryType)
+	layout, err := t.layout()
+	if err != nil {
+		return nil, err
+	}
+
+	switch t {
+	case Point, PointZ, PointM, PointZM:
+		flatCoords, err := readFlatCoords0(r, byteOrder, layout.Stride())
+		if err != nil {
+			return nil, err
+		}
+		return geom.NewPointFlat(layout, flatCoords), nil
+	case LineString, LineStringZ, LineStringM, LineStringZM:
+		flatCoords, err := readFlatCoords1(r, byteOrder, layout.Stride())
+		if err != nil {
+			return nil, err
+		}
+		return geom.NewLineStringFlat(layout, flatCoords), nil
+	case Polygon, PolygonZ, PolygonM, PolygonZM:
+		flatCoords, ends, err := readFlatCoords2(r, byteOrder, layout.Stride())
+		if err != nil {
+			return nil, err
+		}
+		return geom.NewPolygonFlat(layout, flatCoords, ends), nil
+	case MultiPoint, MultiPointZ, MultiPointM, MultiPointZM:
+		var n uint32
+		if err := binary.Read(r, byteOrder, &n); err != nil {
+			return nil, err
+		}
+		mp, _ := geom.NewMultiPoint(layout, nil)
+		for i := uint32(0); i < n; i++ {
+			g, err := read(r)
+			if err != nil {
+				return nil, err
+			}
+			p, ok := g.(*geom.Point)
+			if !ok {
+				return nil, fmt.Errorf("wkb: got a %T, want *geom.Point", g)
+			}
+			if err = mp.Push(p); err != nil {
+				return nil, err
+			}
+		}
+		return mp, nil
+	default:
+		return nil, ErrUnsupportedType(wkbGeometryType)
+	}
+
+}
+
+func Read(data []byte) (geom.T, error) {
+	return read(bytes.NewBuffer(data))
+}
+
+func writeFlatCoords0(w io.Writer, byteOrder ByteOrder, coord []float64) error {
+	return binary.Write(w, byteOrder, coord)
+}
+
+func writeFlatCoords1(w io.Writer, byteOrder ByteOrder, coords []float64, stride int) error {
+	if err := binary.Write(w, byteOrder, uint32(len(coords)/stride)); err != nil {
+		return err
+	}
+	return binary.Write(w, byteOrder, coords)
+}
+
+func writeFlatCoords2(w io.Writer, byteOrder ByteOrder, flatCoords []float64, ends []int, stride int) error {
+	if err := binary.Write(w, byteOrder, uint32(len(ends))); err != nil {
+		return err
+	}
+	offset := 0
+	for _, end := range ends {
+		if err := writeFlatCoords1(w, byteOrder, flatCoords[offset:end], stride); err != nil {
+			return err
+		}
+		offset = end
+	}
+	return nil
+}
+
+func write(w io.Writer, byteOrder ByteOrder, g geom.T) error {
+
+	var wkbByteOrder byte
+	switch byteOrder {
+	case XDR:
+		wkbByteOrder = wkbXDR
+	case NDR:
+		wkbByteOrder = wkbNDR
+	default:
+		panic("wkb: invalid byte order") // FIXME
+	}
+	if err := binary.Write(w, byteOrder, wkbByteOrder); err != nil {
+		return err
+	}
+
+	var wkbGeometryType uint32
+	switch g.(type) {
+	case *geom.Point:
+		wkbGeometryType = wkbPoint
+	case *geom.LineString:
+		wkbGeometryType = wkbLineString
+	case *geom.Polygon:
+		wkbGeometryType = wkbPolygon
+	case *geom.MultiPoint:
+		wkbGeometryType = wkbMultiPoint
+	case *geom.MultiLineString:
+		wkbGeometryType = wkbMultiLineString
+	case *geom.MultiPolygon:
+		wkbGeometryType = wkbPolygon
+	default:
+		panic("wkb: unsupported type") // FIXME
+	}
+	switch g.Layout() {
+	case geom.XY:
+		wkbGeometryType += wkbXY
+	case geom.XYZ:
+		wkbGeometryType += wkbXYZ
+	case geom.XYM:
+		wkbGeometryType += wkbXYM
+	case geom.XYZM:
+		wkbGeometryType += wkbXYZM
+	default:
+		panic("wkb: unsupported layout") // FIXME
+	}
+	if err := binary.Write(w, byteOrder, wkbGeometryType); err != nil {
+		return err
+	}
+
+	switch g.(type) {
+	case *geom.Point:
+		return writeFlatCoords0(w, byteOrder, g.FlatCoords())
+	case *geom.LineString:
+		return writeFlatCoords1(w, byteOrder, g.FlatCoords(), g.Stride())
+	case *geom.Polygon:
+		return writeFlatCoords2(w, byteOrder, g.FlatCoords(), g.Ends(), g.Stride())
+	case *geom.MultiPoint:
+		mp := g.(*geom.MultiPoint)
+		n := mp.NumPoints()
+		if err := binary.Write(w, byteOrder, uint32(n)); err != nil {
+			return err
+		}
+		for i := 0; i < n; i++ {
+			if err := write(w, byteOrder, mp.Point(i)); err != nil {
+				return err
+			}
+		}
+		return nil
+	case *geom.MultiLineString:
+		mp := g.(*geom.MultiLineString)
+		n := mp.NumLineStrings()
+		if err := binary.Write(w, byteOrder, uint32(n)); err != nil {
+			return err
+		}
+		for i := 0; i < n; i++ {
+			if err := write(w, byteOrder, mls.LineString(i)); err != nil {
+				return err
+			}
+		}
+		return nil
+	case *geom.MultiPolygon:
+		mp := g.(*geom.MultiPolygon)
+		n := mp.NumPolygons()
+		if err := binary.Write(w, byteOrder, uint32(n)); err != nil {
+			return err
+		}
+		for i := 0; i < n; i++ {
+			if err := write(w, byteOrder, mp.Polygon(i)); err != nil {
+				return err
+			}
+		}
+	default:
+		return fmt.Errorf("wkb: unsupported or unknown geometry type: %T", g)
+	}
+
+}
+
+func Write(g geom.T, byteOrder ByteOrder) ([]byte, error) {
+	w := bytes.NewBuffer(nil)
+	if err := write(w, byteOrder, g); err != nil {
+		return nil, err
+	}
+	return w.Bytes(), nil
+}
