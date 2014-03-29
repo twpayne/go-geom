47edbeb6169e8d6b56f3f1caf8227378054a7da8
Tom Payne <twpayne@gmail.com>
2014-03-29 21:00:59 +0100

    Add Polygon

---
+++ geom/polygon.go
@@ -0,0 +1,99 @@
+package geom
+
+type Polygon struct {
+	layout     Layout
+	stride     int
+	flatCoords []float64
+	ends       []int
+}
+
+var _ T = &Polygon{}
+
+func NewPolygon(layout Layout, coords2 [][][]float64) (*Polygon, error) {
+	p := &Polygon{
+		layout:     layout,
+		stride:     layout.Stride(),
+		flatCoords: nil,
+		ends:       nil,
+	}
+	var err error
+	if p.flatCoords, p.ends, err = deflate2(p.flatCoords, p.ends, coords2, p.stride); err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func NewPolygonFlat(layout Layout, flatCoords []float64, ends []int) *Polygon {
+	return &Polygon{
+		layout:     layout,
+		stride:     layout.Stride(),
+		flatCoords: flatCoords,
+		ends:       ends,
+	}
+}
+
+func (p *Polygon) Clone() *Polygon {
+	flatCoords := make([]float64, len(p.flatCoords))
+	copy(flatCoords, p.flatCoords)
+	ends := make([]int, len(p.ends))
+	copy(ends, p.ends)
+	return &Polygon{
+		layout:     p.layout,
+		stride:     p.stride,
+		flatCoords: flatCoords,
+		ends:       ends,
+	}
+}
+
+func (p *Polygon) Coords() interface{} {
+	return inflate2(p.flatCoords, 0, p.ends, p.stride)
+}
+
+func (p *Polygon) Ends() []int {
+	return p.ends
+}
+
+func (p *Polygon) Endss() [][]int {
+	return nil
+}
+
+func (p *Polygon) Envelope() *Envelope {
+	return NewEnvelope().extendFlatCoords(p.flatCoords, 0, len(p.flatCoords), p.stride)
+}
+
+func (p *Polygon) FlatCoords() []float64 {
+	return p.flatCoords
+}
+
+func (p *Polygon) Layout() Layout {
+	return p.layout
+}
+
+func (p *Polygon) LinearRing(i int) *LinearRing {
+	offset := 0
+	if i > 0 {
+		offset = p.ends[i-1]
+	}
+	return &LinearRing{
+		layout:     p.layout,
+		stride:     p.stride,
+		flatCoords: p.flatCoords[offset:p.ends[i]],
+	}
+}
+
+func (p *Polygon) NumLinearRings() int {
+	return len(p.ends)
+}
+
+func (p *Polygon) Push(lr *LinearRing) error {
+	if lr.layout != p.layout {
+		return ErrLayoutMismatch{Got: lr.layout, Want: p.layout}
+	}
+	p.flatCoords = append(p.flatCoords, lr.flatCoords...)
+	p.ends = append(p.ends, len(p.flatCoords))
+	return nil
+}
+
+func (p *Polygon) Stride() int {
+	return p.stride
+}
---
+++ geom/polygon_test.go
@@ -0,0 +1,59 @@
+package geom
+
+import (
+	. "launchpad.net/gocheck"
+)
+
+type PolygonSuite struct{}
+
+var _ = Suite(&PolygonSuite{})
+
+func (s *PolygonSuite) TestXY(c *C) {
+
+	coords2 := [][][]float64{{{1, 2}, {3, 4}, {5, 6}}, {{7, 8}, {9, 10}, {11, 12}}}
+	p, err := NewPolygon(XY, coords2)
+	c.Assert(err, IsNil)
+	c.Assert(p, Not(IsNil))
+
+	c.Check(p.Coords(), DeepEquals, coords2)
+	c.Check(p.Envelope(), DeepEquals, NewEnvelope(1, 2, 11, 12))
+	c.Check(p.Layout(), Equals, XY)
+	c.Check(p.NumLinearRings(), Equals, 2)
+	c.Check(p.Stride(), Equals, 2)
+
+	c.Check(p.Ends(), DeepEquals, []int{6, 12})
+	c.Check(p.Endss(), IsNil)
+	c.Check(p.FlatCoords(), DeepEquals, []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12})
+
+	lr0 := p.LinearRing(0)
+	c.Check(lr0, FitsTypeOf, &LinearRing{})
+	c.Check(lr0.Coords(), DeepEquals, coords2[0])
+	c.Check(lr0.Envelope(), DeepEquals, NewEnvelope(1, 2, 5, 6))
+	c.Check(lr0.FlatCoords(), Aliases, p.FlatCoords())
+	c.Check(lr0.Layout(), Equals, p.Layout())
+	c.Check(lr0.Stride(), Equals, p.Stride())
+
+	lr1 := p.LinearRing(1)
+	c.Check(lr1, FitsTypeOf, &LinearRing{})
+	c.Check(lr1.Coords(), DeepEquals, coords2[1])
+	c.Check(lr1.Envelope(), DeepEquals, NewEnvelope(7, 8, 11, 12))
+	c.Check(lr1.FlatCoords(), Aliases, p.FlatCoords())
+	c.Check(lr1.Layout(), Equals, p.Layout())
+	c.Check(lr1.Stride(), Equals, p.Stride())
+
+}
+
+func (s *PolygonSuite) TestClone(c *C) {
+	coords2 := [][][]float64{{{1, 2}, {3, 4}, {5, 6}}, {{7, 8}, {9, 10}, {11, 12}}}
+	p1, err := NewPolygon(XY, coords2)
+	c.Assert(err, IsNil)
+	c.Assert(p1, Not(IsNil))
+	p2 := p1.Clone()
+	c.Check(p2, Not(Equals), p1)
+	c.Check(p2.Coords(), DeepEquals, p1.Coords())
+	//c.Check(p2.Ends(), Not(Aliases), p1.Ends())
+	c.Check(p2.Envelope(), DeepEquals, p1.Envelope())
+	c.Check(p2.FlatCoords(), Not(Aliases), p1.FlatCoords())
+	c.Check(p2.Layout(), Equals, p1.Layout())
+	c.Check(p2.Stride(), Equals, p1.Stride())
+}
