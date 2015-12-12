9769174affb8ee2402558e85147ec62c8c93d697
Tom Payne <twpayne@gmail.com>
2015-12-12 22:18:23 +0100

    Add initial Point documentation

--- point.go
+++ point.go
@@ -1,42 +1,50 @@
 package geom
 
+// A Point represents a single point.
 type Point struct {
 	geom0
 }
 
 var _ T = &Point{}
 
-func NewPoint(layout Layout) *Point {
-	return NewPointFlat(layout, make([]float64, layout.Stride()))
+// NewPoint allocates a new Point with layout l and all values zero.
+func NewPoint(l Layout) *Point {
+	return NewPointFlat(l, make([]float64, l.Stride()))
 }
 
-func NewPointFlat(layout Layout, flatCoords []float64) *Point {
+// NewPointFlat allocates a new Point with layout l and flat coordinates flatCoords.
+func NewPointFlat(l Layout, flatCoords []float64) *Point {
 	p := new(Point)
-	p.layout = layout
-	p.stride = layout.Stride()
+	p.layout = l
+	p.stride = l.Stride()
 	p.flatCoords = flatCoords
 	return p
 }
 
+// Area returns p's area, i.e. zero.
 func (p *Point) Area() float64 {
 	return 0
 }
 
+// Clone returns a copy of p that does not alias p.
 func (p *Point) Clone() *Point {
 	flatCoords := make([]float64, len(p.flatCoords))
 	copy(flatCoords, p.flatCoords)
 	return NewPointFlat(p.layout, flatCoords)
 }
 
+// Length returns the length of p, i.e. zero.
 func (p *Point) Length() float64 {
 	return 0
 }
 
+// MustSetCoords is like SetCoords but panics on any error.
 func (p *Point) MustSetCoords(coords Coord) *Point {
 	Must(p.SetCoords(coords))
 	return p
 }
 
+// SetCoords sets the coordinates of p.
 func (p *Point) SetCoords(coords Coord) (*Point, error) {
 	if err := p.setCoords(coords); err != nil {
 		return nil, err
