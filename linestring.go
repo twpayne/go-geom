99b5d6fefd5862b7b6ab4dcddbcefd8344785fc8
Tom Payne <twpayne@gmail.com>
2015-12-12 22:18:37 +0100

    Add initial LineString documentation

--- linestring.go
+++ linestring.go
@@ -1,15 +1,20 @@
 package geom
 
+// A LineString represents a single, unbroken line, linearly interpreted
+// between zero or more control points.
 type LineString struct {
 	geom1
 }
 
 var _ T = &LineString{}
 
-func NewLineString(layout Layout) *LineString {
-	return NewLineStringFlat(layout, nil)
+// NewLineString returns a new LineString with layout l and no control points.
+func NewLineString(l Layout) *LineString {
+	return NewLineStringFlat(l, nil)
 }
 
+// NewLineStringFlat returns a new LineString with layout l and control points
+// flatCoords.
 func NewLineStringFlat(layout Layout, flatCoords []float64) *LineString {
 	ls := new(LineString)
 	ls.layout = layout
@@ -18,10 +23,12 @@ func NewLineStringFlat(layout Layout, flatCoords []float64) *LineString {
 	return ls
 }
 
+// Area returns the length of ls, i.e. zero.
 func (ls *LineString) Area() float64 {
 	return 0
 }
 
+// Clone returns a copy of ls that does not alias ls.
 func (ls *LineString) Clone() *LineString {
 	flatCoords := make([]float64, len(ls.flatCoords))
 	copy(flatCoords, ls.flatCoords)
@@ -58,15 +65,18 @@ func (ls *LineString) Interpolate(val float64, dim int) (int, float64) {
 	return low, (val - val0) / (val1 - val0)
 }
 
+// Length returns the length of ls.
 func (ls *LineString) Length() float64 {
 	return length1(ls.flatCoords, 0, len(ls.flatCoords), ls.stride)
 }
 
+// MustSetCoords is like SetCoords but it panics on any error.
 func (ls *LineString) MustSetCoords(coords []Coord) *LineString {
 	Must(ls.SetCoords(coords))
 	return ls
 }
 
+// SetCoords sets the coordinates of ls.
 func (ls *LineString) SetCoords(coords []Coord) (*LineString, error) {
 	if err := ls.setCoords(coords); err != nil {
 		return nil, err
@@ -74,6 +84,8 @@ func (ls *LineString) SetCoords(coords []Coord) (*LineString, error) {
 	return ls, nil
 }
 
+// SubLineString returns a LineString from starts at index start and stops at
+// index stop of ls. The returned LineString aliases ls.
 func (ls *LineString) SubLineString(start, stop int) *LineString {
 	return NewLineStringFlat(ls.layout, ls.flatCoords[start*ls.stride:stop*ls.stride])
 }
