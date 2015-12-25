e6de632f9246cc38a199dbe89f157a92c2f4798f
Tom Payne <twpayne@gmail.com>
2015-12-25 14:35:57 +0000

    Refactor GeoJSON support

--- encoding/geojson/geojson_test.go
+++ encoding/geojson/geojson_test.go
@@ -9,110 +9,96 @@ import (
 
 func TestGeometry(t *testing.T) {
 	for _, tc := range []struct {
-		g        geom.T
-		geometry *Geometry
-		s        string
+		g geom.T
+		s string
 	}{
 		{
 			g: geom.NewPoint(DefaultLayout),
-			geometry: &Geometry{
-				Type:        "Point",
-				Coordinates: geom.Coord{0, 0},
-			},
 			s: `{"type":"Point","coordinates":[0,0]}`,
 		},
 		{
 			g: geom.NewPoint(geom.XY).MustSetCoords(geom.Coord{1, 2}),
-			geometry: &Geometry{
-				Type:        "Point",
-				Coordinates: geom.Coord{1, 2},
-			},
 			s: `{"type":"Point","coordinates":[1,2]}`,
 		},
 		{
 			g: geom.NewPoint(geom.XYZ).MustSetCoords(geom.Coord{1, 2, 3}),
-			geometry: &Geometry{
-				Type:        "Point",
-				Coordinates: geom.Coord{1, 2, 3},
-			},
 			s: `{"type":"Point","coordinates":[1,2,3]}`,
 		},
 		{
 			g: geom.NewPoint(geom.XYZM).MustSetCoords(geom.Coord{1, 2, 3, 4}),
-			geometry: &Geometry{
-				Type:        "Point",
-				Coordinates: geom.Coord{1, 2, 3, 4},
-			},
 			s: `{"type":"Point","coordinates":[1,2,3,4]}`,
 		},
 		{
 			g: geom.NewLineString(DefaultLayout),
-			geometry: &Geometry{
-				Type:        "LineString",
-				Coordinates: []geom.Coord{},
-			},
 			s: `{"type":"LineString","coordinates":[]}`,
 		},
 		{
 			g: geom.NewLineString(geom.XY).MustSetCoords([]geom.Coord{{1, 2}, {3, 4}}),
-			geometry: &Geometry{
-				Type:        "LineString",
-				Coordinates: []geom.Coord{{1, 2}, {3, 4}},
-			},
 			s: `{"type":"LineString","coordinates":[[1,2],[3,4]]}`,
 		},
 		{
 			g: geom.NewLineString(geom.XYZ).MustSetCoords([]geom.Coord{{1, 2, 3}, {4, 5, 6}}),
-			geometry: &Geometry{
-				Type:        "LineString",
-				Coordinates: []geom.Coord{{1, 2, 3}, {4, 5, 6}},
-			},
 			s: `{"type":"LineString","coordinates":[[1,2,3],[4,5,6]]}`,
 		},
 		{
 			g: geom.NewLineString(geom.XYZM).MustSetCoords([]geom.Coord{{1, 2, 3, 4}, {5, 6, 7, 8}}),
-			geometry: &Geometry{
-				Type:        "LineString",
-				Coordinates: []geom.Coord{{1, 2, 3, 4}, {5, 6, 7, 8}},
-			},
 			s: `{"type":"LineString","coordinates":[[1,2,3,4],[5,6,7,8]]}`,
 		},
 		{
 			g: geom.NewPolygon(DefaultLayout),
-			geometry: &Geometry{
-				Type:        "Polygon",
-				Coordinates: [][]geom.Coord{},
-			},
 			s: `{"type":"Polygon","coordinates":[]}`,
 		},
 		{
 			g: geom.NewPolygon(geom.XY).MustSetCoords([][]geom.Coord{{{1, 2}, {3, 4}, {5, 6}, {1, 2}}}),
-			geometry: &Geometry{
-				Type:        "Polygon",
-				Coordinates: [][]geom.Coord{{{1, 2}, {3, 4}, {5, 6}, {1, 2}}},
-			},
 			s: `{"type":"Polygon","coordinates":[[[1,2],[3,4],[5,6],[1,2]]]}`,
 		},
 		{
 			g: geom.NewPolygon(geom.XYZ).MustSetCoords([][]geom.Coord{{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 2, 3}}}),
-			geometry: &Geometry{
-				Type:        "Polygon",
-				Coordinates: [][]geom.Coord{{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 2, 3}}},
-			},
 			s: `{"type":"Polygon","coordinates":[[[1,2,3],[4,5,6],[7,8,9],[1,2,3]]]}`,
 		},
-		// FIXME Add MultiPoint tests
-		// FIXME Add MultiLineString tests
-		// FIXME Add MultiPolygon tests
+		{
+			g: geom.NewMultiPoint(DefaultLayout),
+			s: `{"type":"MultiPoint","coordinates":[]}`,
+		},
+		{
+			g: geom.NewMultiPoint(geom.XY).MustSetCoords([]geom.Coord{{1, 2}, {3, 4}}),
+			s: `{"type":"MultiPoint","coordinates":[[1,2],[3,4]]}`,
+		},
+		{
+			g: geom.NewMultiPoint(geom.XYZ).MustSetCoords([]geom.Coord{{1, 2, 3}, {4, 5, 6}}),
+			s: `{"type":"MultiPoint","coordinates":[[1,2,3],[4,5,6]]}`,
+		},
+		{
+			g: geom.NewMultiPoint(geom.XYZM).MustSetCoords([]geom.Coord{{1, 2, 3, 4}, {5, 6, 7, 8}}),
+			s: `{"type":"MultiPoint","coordinates":[[1,2,3,4],[5,6,7,8]]}`,
+		},
+		{
+			g: geom.NewMultiLineString(DefaultLayout),
+			s: `{"type":"MultiLineString","coordinates":[]}`,
+		},
+		{
+			g: geom.NewMultiLineString(geom.XY).MustSetCoords([][]geom.Coord{{{1, 2}, {3, 4}, {5, 6}, {1, 2}}}),
+			s: `{"type":"MultiLineString","coordinates":[[[1,2],[3,4],[5,6],[1,2]]]}`,
+		},
+		{
+			g: geom.NewMultiLineString(geom.XYZ).MustSetCoords([][]geom.Coord{{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 2, 3}}}),
+			s: `{"type":"MultiLineString","coordinates":[[[1,2,3],[4,5,6],[7,8,9],[1,2,3]]]}`,
+		},
+		{
+			g: geom.NewMultiPolygon(DefaultLayout),
+			s: `{"type":"MultiPolygon","coordinates":[]}`,
+		},
+		{
+			g: geom.NewMultiPolygon(geom.XYZ).MustSetCoords([][][]geom.Coord{{{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 2, 3}}, {{-1, -2, -3}, {-4, -5, -6}, {-7, -8, -9}, {-1, -2, -3}}}}),
+			s: `{"type":"MultiPolygon","coordinates":[[[[1,2,3],[4,5,6],[7,8,9],[1,2,3]],[[-1,-2,-3],[-4,-5,-6],[-7,-8,-9],[-1,-2,-3]]]]}`,
+		},
 	} {
-		if got, err := Encode(tc.g); err != nil || !reflect.DeepEqual(got, tc.geometry) {
-			t.Errorf("Encode(%#v) == %#v, %v, want %#v, nil", tc.g, got, err, tc.geometry)
-		}
-		if got, err := Decode(tc.geometry); err != nil || !reflect.DeepEqual(got, tc.g) {
-			t.Errorf("Decode(%#v) == %#v, %v, want %#v, nil", tc.geometry, got, err, tc.g)
-		}
 		if got, err := Marshal(tc.g); err != nil || string(got) != tc.s {
 			t.Errorf("Marshal(%#v) == %#v, %v, want %#v, nil", tc.g, string(got), err, tc.s)
+		}
+		var g geom.T
+		if err := Unmarshal([]byte(tc.s), &g); err != nil || !reflect.DeepEqual(g, tc.g) {
+			t.Errorf("Unmarshal(%#v, %#v) == %v, want %#v, nil", tc.s, g, err, tc.g)
 		}
 	}
 }
