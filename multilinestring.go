aa9dcc6fb378d81f8de9eba52a38aac70b9eee4c
Tom Payne <twpayne@gmail.com>
2016-02-27 18:33:47 +0100

    Add some MultiLineString documentation

--- multilinestring.go
+++ multilinestring.go
@@ -1,13 +1,16 @@
 package geom
 
+// A MultiLineString is a collection of LineStrings.
 type MultiLineString struct {
 	geom2
 }
 
+// NewMultiLineString returns a new MultiLineString with no LineStrings.
 func NewMultiLineString(layout Layout) *MultiLineString {
 	return NewMultiLineStringFlat(layout, nil, nil)
 }
 
+// NewMultiLineStringFlat returns a new MultiLineString with the given flat coordinates.
 func NewMultiLineStringFlat(layout Layout, flatCoords []float64, ends []int) *MultiLineString {
 	mls := new(MultiLineString)
 	mls.layout = layout
@@ -17,10 +20,12 @@ func NewMultiLineStringFlat(layout Layout, flatCoords []float64, ends []int) *Mu
 	return mls
 }
 
+// Area returns 0.
 func (mls *MultiLineString) Area() float64 {
 	return 0
 }
 
+// Clone returns a deep copy.
 func (mls *MultiLineString) Clone() *MultiLineString {
 	flatCoords := make([]float64, len(mls.flatCoords))
 	copy(flatCoords, mls.flatCoords)
@@ -29,14 +34,17 @@ func (mls *MultiLineString) Clone() *MultiLineString {
 	return NewMultiLineStringFlat(mls.layout, flatCoords, ends)
 }
 
+// Empty returns true if the collection is empty.
 func (mls *MultiLineString) Empty() bool {
 	return mls.NumLineStrings() == 0
 }
 
+// Length returns the sum of the length of the LineStrings.
 func (mls *MultiLineString) Length() float64 {
 	return length2(mls.flatCoords, 0, mls.ends, mls.stride)
 }
 
+// LineString returns the ith LineString.
 func (mls *MultiLineString) LineString(i int) *LineString {
 	offset := 0
 	if i > 0 {
@@ -45,15 +53,18 @@ func (mls *MultiLineString) LineString(i int) *LineString {
 	return NewLineStringFlat(mls.layout, mls.flatCoords[offset:mls.ends[i]])
 }
 
+// MustSetCoords sets the coordinates and panics on any error.
 func (mls *MultiLineString) MustSetCoords(coords [][]Coord) *MultiLineString {
 	Must(mls.SetCoords(coords))
 	return mls
 }
 
+// NumLineStrings returns the number of LineStrings.
 func (mls *MultiLineString) NumLineStrings() int {
 	return len(mls.ends)
 }
 
+// Push appends a LineString.
 func (mls *MultiLineString) Push(ls *LineString) error {
 	if ls.layout != mls.layout {
 		return ErrLayoutMismatch{Got: ls.layout, Want: mls.layout}
@@ -63,6 +74,7 @@ func (mls *MultiLineString) Push(ls *LineString) error {
 	return nil
 }
 
+// SetCoords sets the coordinates.
 func (mls *MultiLineString) SetCoords(coords [][]Coord) (*MultiLineString, error) {
 	if err := mls.setCoords(coords); err != nil {
 		return nil, err
