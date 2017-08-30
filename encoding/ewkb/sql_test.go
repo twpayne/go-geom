7926436b763893e6e5f72c9dbec4c16ece9bde34
Tom Payne <twpayne@gmail.com>
2017-08-30 19:33:21 +0200

    ewkb: support NULL values

---
+++ encoding/ewkb/sql_test.go
@@ -0,0 +1,69 @@
+package ewkb
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"encoding/hex"
+	"reflect"
+	"testing"
+
+	"github.com/twpayne/go-geom"
+)
+
+var (
+	_ = []interface {
+		sql.Scanner
+		Value() (driver.Value, error)
+		Valid() bool
+	}{
+		&Point{},
+		&LineString{},
+		&Polygon{},
+		&MultiPoint{},
+		&MultiLineString{},
+		&MultiPolygon{},
+		&GeometryCollection{},
+	}
+)
+
+func mustHexDecode(s string) []byte {
+	data, err := hex.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+func TestPointScanAndValue(t *testing.T) {
+	for _, tc := range []struct {
+		value interface{}
+		point Point
+		valid bool
+	}{
+		{
+			value: nil,
+			point: Point{Point: nil},
+			valid: false,
+		},
+		{
+			value: mustHexDecode("0101000000000000000000f03f0000000000000040"),
+			point: Point{Point: geom.NewPoint(geom.XY).MustSetCoords(geom.Coord{1, 2})},
+			valid: true,
+		},
+	} {
+		var gotPoint Point
+		if gotErr := gotPoint.Scan(tc.value); gotErr != nil {
+			t.Errorf("gotPoint.Scan(%v) == %v, want <nil>", tc.value, gotErr)
+		}
+		if !reflect.DeepEqual(gotPoint, tc.point) {
+			t.Errorf("gotPoint.Scan(%v); gotPoint == %v, want == %v", value, gotPoint, tc.point)
+		}
+		if gotPointValid := gotPoint.Valid(); gotPointValid != tc.valid {
+			t.Errorf("gotPoint.Scan(%v); gotPoint.Valid() == %t, want %t", value, gotPointValid, tc.valid)
+		}
+		gotValue, gotErr := tc.point.Value()
+		if gotErr != nil || !reflect.DeepEqual(gotValue, tc.value) {
+			t.Errorf("%v.Value() == %v, %v, want %v, <nil>", tc.point, gotValue, gotErr, tc.value)
+		}
+	}
+}
