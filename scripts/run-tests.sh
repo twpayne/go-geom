947c0d0113fd51c4a720422ce394bde7f6c0e174
Tom Payne <twpayne@gmail.com>
2017-09-10 11:03:43 +0200

    Explicitly test on go 1.9

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,11 +5,14 @@ set -e -x
 goversion=$(go version | cut -d ' ' -f 3)
 
 if [ ! "${goversion}" \< "go1.8" ] ; then
+	git diff
 	git diff-index --quiet HEAD --
 	go fmt ./...
+	git diff
 	git diff-index --quiet HEAD --
 	go get github.com/awalterschulze/goderive
 	go generate ./...
+	git diff
 	git diff-index --quiet HEAD --
 fi
 
