8683299c32aca6e9846504901d14b16965b9cda8
Tom Payne <twpayne@gmail.com>
2024-07-29 18:47:05 +0200

    Relax precision test in example

--- xy/line_centroid_examples_test.go
+++ xy/line_centroid_examples_test.go
@@ -19,7 +19,7 @@ func ExampleLinearRingsCentroid() {
 	line1 := geom.NewLinearRingFlat(geom.XY, []float64{0, 0, 1, 1, 3, 3, 0, 0})
 	line2 := geom.NewLinearRingFlat(geom.XY, []float64{10, 10, 11, 11, 13, 13, 10, 10})
 	centroid := xy.LinearRingsCentroid(line1, line2)
-	fmt.Println(centroid)
+	fmt.Printf("[%.1f %.1f]\n", centroid.X(), centroid.Y())
 	// Output: [6.5 6.5]
 }
 
